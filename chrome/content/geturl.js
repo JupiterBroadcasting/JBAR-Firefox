e03396b9ea10fd9caec6b281dc163f1134f0eb8d
Richard Korman <winter.blood@gmail.com>
2012-09-12 17:06:08 -0400

    Fixed some false url matching issues.
    Added \. before domains so they would only match for the domains they
    were meant for, as well as subdomains. For example, the mint affiliate
    no longer matches on linuxmint

--- chrome/content/geturl.js
+++ chrome/content/geturl.js
@@ -7,16 +7,16 @@
 				url = aLocation.spec;
 				tag = null;
 				sites = {
-							amazon:     { rx: /^http.*?amazon.com.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=thelinactsho-20"},
-							amazonuk:   { rx: /^http.*?amazon.co.uk.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=jupitebroadc-21"}, 
-							amazonde:   { rx: /^http.*?amazon.de.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=jupitebroad02-21"}, 
-							audible:    { rx: /^http.*?audible.com/i, tag:"source_code=COMA0230WS012110&AID=10298646&PID=4897915"}, 
-							mint:       { rx: /^http.*?mint.com/i, tag:"PID=4897915&priorityCode=4216102399&source=cj_pfm"},
-							bestbuy:    { rx: /^http.*?bestbuy.com.site.*?\?id=/i, tag:"AID=10483113&PID=4897915&ref=39&CJPID=4897915&loc=01"},
-							thinkgeek:  { rx: /^http.*?thinkgeek.com/i, tag:"cpg=cj&ref=&CJURL=&CJID=3282554"},
-							neweggcom:  { rx: /^http.*?newegg.com.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i, tag:"nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-_-Branding-_-na-_-na&AID=10440554&PID=4897915"},
-							neweggca:	{ rx: /^http.*?newegg.ca.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i, tag:"nm_mc=AFC-C8junctionCA&cm_mmc=AFC-C8JunctionCA-_-homepage-_-na-_-na&AID=10606701&PID=4897915" },
-                            guitarcenter:   { rx: /^http.*?guitarcenter.com/i, tag:"CJAID=10453836&CJPID=4897915"}
+							amazon:     { rx: /^http.*?\.amazon.com.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=thelinactsho-20"},
+							amazonuk:   { rx: /^http.*?\.amazon.co.uk.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=jupitebroadc-21"}, 
+							amazonde:   { rx: /^http.*?\.amazon.de.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, tag:"tag=jupitebroad02-21"}, 
+							audible:    { rx: /^http.*?\.audible.com/i, tag:"source_code=COMA0230WS012110&AID=10298646&PID=4897915"}, 
+							mint:       { rx: /^http.*?\.mint.com/i, tag:"PID=4897915&priorityCode=4216102399&source=cj_pfm"},
+							bestbuy:    { rx: /^http.*?\.bestbuy.com.site.*?\?id=/i, tag:"AID=10483113&PID=4897915&ref=39&CJPID=4897915&loc=01"},
+							thinkgeek:  { rx: /^http.*?\.thinkgeek.com/i, tag:"cpg=cj&ref=&CJURL=&CJID=3282554"},
+							neweggcom:  { rx: /^http.*?\.newegg.com.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i, tag:"nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-_-Branding-_-na-_-na&AID=10440554&PID=4897915"},
+							neweggca:	{ rx: /^http.*?\.newegg.ca.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i, tag:"nm_mc=AFC-C8junctionCA&cm_mmc=AFC-C8JunctionCA-_-homepage-_-na-_-na&AID=10606701&PID=4897915" },
+							guitarcenter:   { rx: /^http.*?\.guitarcenter.com/i, tag:"CJAID=10453836&CJPID=4897915"}
                             
 				};			
 				for(s in sites) {
